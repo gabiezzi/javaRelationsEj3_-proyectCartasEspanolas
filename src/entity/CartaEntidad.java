bdb30f2ac3dbb454d3e449b74eb2b313132c9d9b
Gabi iezzi <geiezzi90@gmail.com>
2022-02-16 22:03:00 -0300

    primerCommit : add entity, service and implement packages

---
+++ src/entity/CartaEntidad.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Gabi
+ */
+public class CartaEntidad {
+    
+    private String palo;
+    private int numero;
+
+    public CartaEntidad(String palo, int numero) {
+        this.palo = palo;
+        this.numero = numero;
+    }
+
+    public CartaEntidad() {
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Carta [" + palo + " , " + numero + ']';
+    }
+    
+    
+    
+}
